76b2fc598c8cec8cdb446111a3ffc69f117c0b41
kartikgupta666 <kartikgangil@gmail.com>
2024-02-13 23:51:40 +0530

    files uploaded alert feature implemented

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import Textform from './component/Textform';
+import Alert from './component/Alert';
 import React, { useState } from 'react'
 
 
 function App() {
 
   const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState( null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      types: type
+    })
+  }
+
   const toggelmode = () => {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#3a3c3e';
       document.body.style.textColor = '#FFF';
-
+      showAlert("Dark mode has been enabled", " success");
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = '#FFF';
+      showAlert("Light mode has been enabled", " success");
 
     }
   }
@@ -25,8 +36,9 @@ function App() {
   return (
     <>
       <Navbar mode={mode} toggelmode={toggelmode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <Textform  mode={mode} heading="Enter the text to analyse below" />
+        <Textform mode={mode} heading="Enter the text to analyse below" />
       </div>
     </>
   );
